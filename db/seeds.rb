4ab76cc9c057cb7548326a6ec7fde480cb24475c
Cameron Melville <cam.t.melville@gmail.com>
2020-02-19 14:01:07 +1100

    basic version completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "Mc Donalds",address: "1 imagine street", phone_number: "0395827563", category: "italian")
+Restaurant.create(name: "Wes's House",address: "2 imagine street", phone_number: "029478275", category: "belgian")
+Restaurant.create(name: "Cam's house",address: "7 imagine street", phone_number: "856387204", category: "japanese")
+Restaurant.create(name: "paal's good times",address: "10 le wagon street", phone_number: "589375685", category: "belgian")
+Restaurant.create(name: "Li's favourite food",address: "10 china road", phone_number: "2672946324", category: "chinese")
